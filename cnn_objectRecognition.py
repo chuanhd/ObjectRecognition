c7e969ddeb909d5546541fceb0ae9d94ecd1633d
ChuanHD <hodanhchuan@gmail.com>
2017-09-18 21:49:35 +0700

    Label encode

--- cnn_objectRecognition.py
+++ cnn_objectRecognition.py
@@ -1,10 +1,12 @@
 import numpy as np
-from scipy import misc
+import pandas as pd
 import tensorflow as tf
+
+from scipy import misc
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def loadData():
+def loadImageData():
     # Make a queue of file names including all the PNG images files in the relative
     # image directory.
     filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once("./train_small/*.png"))
@@ -14,7 +16,6 @@ def loadData():
     # reader to split up the file.
     image_reader = tf.WholeFileReader()
 
-
     # Read a whole file from the queue, the first returned value in the tuple is the
     # filename which we are ignoring.
     _, image_file = image_reader.read(filename_queue)
@@ -23,18 +24,47 @@ def loadData():
     # then use in training.
     image = tf.image.decode_png(image_file)
 
-    print(image.shape)
+    # print(image.shape)
 
-    return 
+    return image
+
+def loadTrainCSVData():
+
+    labelMappingDict = {'airplane' : 0, 'automobile' : 1, 'bird' : 2, 'cat' : 3, 'deer' : 4, 'dog' : 5, 'frog' : 6, 'horse' : 7, 'ship' : 8, 'truck' : 9}
+
+    df = pd.read_csv("./trainLabels.csv")
+
+    features = df.iloc[:, :-1].values
+    labels = df.iloc[:, -1].values
+    label_encoder = lambda l: labelMappingDict[l]
+    labels = np.fromiter((label_encoder(label) for label in labels), np.int32)
+    labels = tf.one_hot(labels, 10)
+
+    print("labels: %s" % (labels[1]))
+
+    return (features, labels)
 
 def cnn_model_fn(_features, _labels, _mode):
+
+    # input layer
+    input_layer = tf.reshape()
+
     return
 
 def main(_):
-    # Start new session
+    # # Start new session
     with tf.Session() as sess:
         # Required to get the filename matching to run
         tf.global_variables_initializer().run()
-        loadData()
+        images = loadImageData()
+        features, labels = loadTrainCSVData()
+        # sess.run(labels[1])
+
+    # images = loadImageData()
+    # features, labels = loadTrainCSVData()
+    
+
+
+
 if __name__ == "__main__":
     tf.app.run()
