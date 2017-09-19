e0dbd8aa68b02228c4d1b5e5d19f6996237dd545
chuanhd@iMac <chuan_hd@Chuans-iMac-2.local>
2017-09-19 17:41:22 +0700

    Fix read whole images from folders

--- cnn_objectRecognition.py
+++ cnn_objectRecognition.py
@@ -20,13 +20,28 @@ def loadImageData():
     # filename which we are ignoring.
     _, image_file = image_reader.read(filename_queue)
 
-    # Decode the image as a JPEG file, this will turn it into a Tensor which we can
+    # Decode the image as a PNG file, this will turn it into a Tensor which we can
     # then use in training.
-    image = tf.image.decode_png(image_file)
+    image = tf.image.decode_png(image_file, channels=3)
 
     # print(image.shape)
+    with tf.Session() as sess:
+        # Required to get the filename matching to run
+        tf.local_variables_initializer().run()
 
-    return image
+        # Coordinate the loading of image files
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        # Get an image tensor and print its value.
+        image_tensor = sess.run([image])
+        print(image_tensor[0].shape)
+
+        # Finish off the filename queue coordinator.
+        coord.request_stop()
+        coord.join(threads)
+
+    return image_tensor
 
 def loadTrainCSVData():
 
@@ -35,14 +50,24 @@ def loadTrainCSVData():
     df = pd.read_csv("./trainLabels.csv")
 
     features = df.iloc[:, :-1].values
+
     labels = df.iloc[:, -1].values
     label_encoder = lambda l: labelMappingDict[l]
     labels = np.fromiter((label_encoder(label) for label in labels), np.int32)
     labels = tf.one_hot(labels, 10)
 
-    print("labels: %s" % (labels[1]))
-
     return (features, labels)
+
+def loadTrainingData():
+
+    images  = loadImageData()
+    features, labels = loadTrainCSVData()
+    # image_mapping = lambda i: images[i]
+    # features = np.fromiter((image_mapping(i) for (i, val) in enumerate(images)), np.int32)
+    # for i in range(features.):
+    #     pass
+
+    return
 
 def cnn_model_fn(_features, _labels, _mode):
 
@@ -52,15 +77,7 @@ def cnn_model_fn(_features, _labels, _mode):
     return
 
 def main(_):
-    # # Start new session
-    with tf.Session() as sess:
-        # Required to get the filename matching to run
-        tf.global_variables_initializer().run()
-        images = loadImageData()
-        features, labels = loadTrainCSVData()
-        # sess.run(labels[1])
-
-    # images = loadImageData()
+    images = loadImageData()
     # features, labels = loadTrainCSVData()
     
 
